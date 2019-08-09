33c155b2d9a694898feda145428b30adb3dcaacd
David García Garzón <voki@canvoki.net>
2019-08-09 19:46:07 +0200

    It's not the querystring but the action

--- main.py
+++ main.py
@@ -769,7 +769,7 @@ def router(paramstring):
     dispatchers = { fun.__name__: fun for fun in entrypoints}
     dispatcher = dispatchers.get(action, None)
     if not dispatcher:
-        raise ValueError('Invalid paramstring: {0}!'.format(paramstring))
+        raise ValueError('Invalid action: {0}!'.format(action))
     dispatcher(**params)
 
 if __name__ == '__main__':
